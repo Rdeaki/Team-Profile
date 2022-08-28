dd0f11e6c3d13159e547d02f2df1d28443ddf8fc
rick Deakins <rdeakins96@gmail.com>
2022-08-28 18:50:00 -0400

    playing around with testing

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -5,4 +5,24 @@ test("create the employee object", ()=>{
   
     expect(typeof(e)).toBe("object");
   });
- 
+
+  test("sets the name of the constructor", ()=>{
+    const name = 'ricky';
+    const e = new Employee(name);
+  
+    expect(e.name).toBe(name);
+  });
+  
+  test("sets the id of the constructor", ()=>{
+    const valor = 1;
+    const e = new Employee("ricky", valor);
+  
+    expect(e.id).toBe(valor);
+  });
+  
+  test("sets the email of the constructor", ()=>{
+    const valor = "ricky@gmail.com";
+    const e = new Employee("ricky",1, valor);
+  
+    expect(e.email).toBe(valor);
+  }); 
--- index,js/index.js
+++ index,js/index.js
@@ -1,1 +1,25 @@
 const inquirer = require('inquirer');
+
+function startApp() {
+console.log("welcome to team builder"); 
+
+function createEmployee() {
+    inquirer.prompt([
+        {type: "input",
+        name: "employeeName",
+        message: "whats the employee's name?",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "please enter the name.";
+        }
+        
+    
+    }
+    ]) 
+}
+createEmployee()
+}
+
+startApp();
