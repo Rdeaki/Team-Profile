0105bff7558075613ee0ffaab2b870e1e0a4d657
rick Deakins <rdeakins96@gmail.com>
2022-08-28 20:06:57 -0400

    added template to each test file

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,24 @@
+const engineer = require('../lib/Employee');
+const employee = new Employee('Ricky', '10', 'Ricky@gmail.com');
+
+test('test for engineer info', () => {
+    expect(employee.name).toBe('Ricky');
+    expect(employee.id).toBe('10');
+    expect(employee.email).toBe('Ricky@gmail.com')
+});
+
+test('test if we can get name from getName()', () => {
+    expect(employee.getName()).toBe('Ricky');
+});
+
+test('test if we can get ID from getId()', () => {
+    expect(employee.getId()).toBe('10');
+});
+
+test('test if we can get email from getEmail()', () => {
+    expect(employee.getEmail()).toBe('Ricky@gmail.com');
+});
+
+test('test if we can get role from getRole()', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
