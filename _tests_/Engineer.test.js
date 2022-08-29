77fb4706398f222e7a77d507b08ca911e7b263c5
rick Deakins <rdeakins96@gmail.com>
2022-08-28 20:51:32 -0400

    fixed typo, used employee parent class, engineer now passes

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,9 +1,9 @@
-const NEgineer = require('../lib/Engineer');
+const Engineer = require('../lib/Engineer');
 const engineer = new Engineer('Ricky', '13', 'Ricky@gmail.com', 'rdeaki');
 
 test('test for engineer info', () => {
     expect(engineer.name).toBe('Ricky');
-    expect(engineer.id).toBe('10');
+    expect(engineer.id).toBe('13');
     expect(engineer.email).toBe('Ricky@gmail.com')
     expect(engineer.github)
 });
@@ -20,10 +20,11 @@ test('test if we can get email from getEmail()', () => {
     expect(engineer.getEmail()).toBe('Ricky@gmail.com');
 });
 
-test('test if we can get role from getRole()', () => {
-    expect(engineer.getRole()).toBe('engineer');
-});
 
 test('test for username from getGithub()', () => {
-    expect(engineer.getEmail()).toBe('rdeaki');
+    expect(engineer.getGithub()).toBe('rdeaki');
+});
+
+test('test if we can get role from getRole()', () => {
+    expect(engineer.getRole()).toBe('Engineer');
 });
