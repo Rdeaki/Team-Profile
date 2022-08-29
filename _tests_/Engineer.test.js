9e7bf9c920d5b4a75550e5d3fec1b462406768f1
rick Deakins <rdeakins96@gmail.com>
2022-08-28 20:23:35 -0400

    updated engineer.test, test failing

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,24 +1,29 @@
-const engineer = require('../lib/Employee');
-const employee = new Employee('Ricky', '10', 'Ricky@gmail.com');
+const NEgineer = require('../lib/Engineer');
+const engineer = new Engineer('Ricky', '13', 'Ricky@gmail.com', 'rdeaki');
 
 test('test for engineer info', () => {
-    expect(employee.name).toBe('Ricky');
-    expect(employee.id).toBe('10');
-    expect(employee.email).toBe('Ricky@gmail.com')
+    expect(engineer.name).toBe('Ricky');
+    expect(engineer.id).toBe('10');
+    expect(engineer.email).toBe('Ricky@gmail.com')
+    expect(engineer.github)
 });
 
 test('test if we can get name from getName()', () => {
-    expect(employee.getName()).toBe('Ricky');
+    expect(engineer.getName()).toBe('Ricky');
 });
 
 test('test if we can get ID from getId()', () => {
-    expect(employee.getId()).toBe('10');
+    expect(engineer.getId()).toBe('13');
 });
 
 test('test if we can get email from getEmail()', () => {
-    expect(employee.getEmail()).toBe('Ricky@gmail.com');
+    expect(engineer.getEmail()).toBe('Ricky@gmail.com');
 });
 
 test('test if we can get role from getRole()', () => {
-    expect(employee.getRole()).toBe('Employee');
+    expect(engineer.getRole()).toBe('engineer');
+});
+
+test('test for username from getGithub()', () => {
+    expect(engineer.getEmail()).toBe('rdeaki');
 });
