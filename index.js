baef9d464732803874b8c7ba34f5f949254ae90c
rick Deakins <rdeakins96@gmail.com>
2022-10-04 22:13:43 -0400

    html generated, indexjs test succesful

---
+++ index.js
@@ -0,0 +1,180 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const generatePage = require('./src/generatePage');
+console.log(" Follow along with the prompts to create your team! ");
+
+const teamMembers = [];
+const promptManager = () => {
+    return inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter your Managers name.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('enter your name.')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter your ID number.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('enter your ID number.')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('enter your email.')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'number',
+            message: 'Enter your office number.',
+            validate: numberInput => {
+                if (numberInput) {
+                    return true;
+                } else {
+                    console.log('enter your office number.')
+                    return false;
+                }
+            }
+        },
+
+    ]).then(managerData => {
+        const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.number)
+        teamMembers.push(manager)
+    })
+} 
+
+
+const promptEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'Please select which option you would like to continue with:',
+            choices: [
+                'Engineer',
+                'Intern'
+            ]
+        },
+
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter Employees name.'
+            
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Employees ID.'
+            
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Employees email.'
+            
+        },
+
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter Engineers Github.',
+            when: ({employee}) => {
+                if(employee === 'Engineer'){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            
+        },
+
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter Interns school.',
+            when: ({employee}) => {
+                if(employee === 'Intern'){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            
+        },
+
+        {
+            type: 'list',
+            name: 'confirmaddnew',
+            message: 'Would you like to add another member?',
+            choices: [
+                'Yes',
+                'No'
+            ]
+            
+        },
+
+    ])
+    .then(employeeAnswers => {
+        if(employeeAnswers.employee === 'Engineer') {
+            const newEmployee = new Engineer (employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, employeeAnswers.github);
+            teamMembers.push(newEmployee)
+        } else if (employeeAnswers.employee = 'Intern') {
+            const newEmployee = new Intern (employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, employeeAnswers.school);
+            teamMembers.push(newEmployee)
+        } if(employeeAnswers.confirmaddnew === 'Yes')  return promptEmployee();
+        else if(employeeAnswers.confirmaddnew === 'No') return teamMembers;
+    })
+    
+    
+};
+
+const writeFile = data => {
+    console.log(data);
+    fs.writeFile('./dist/index.html', data, err => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log(' lets gooo Page created. ')
+    })
+}
+
+promptManager()
+.then(promptEmployee)
+.then(teamMembers => generatePage(teamMembers))
+.then(pageHTML => writeFile(pageHTML))
+
---
+++ src/generatePage.js
@@ -0,0 +1,91 @@
+const htmlelements = function(team) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../src/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    
+    <title>Team Profile</title>
+</head>
+<body>
+<div class="container-fluid">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+    
+                <h1 class="display-4">My Team</h1>
+            </div>
+        </div>
+    ${team}
+    
+</body>
+</html>
+    `;
+};
+
+function appendManager(manager){
+    return `
+    <div class="col-md-4">
+    <div class="card">
+    <div class="container">
+    <h2><b>${manager.name}</b></h2></p>
+    <h4><span class="badge">Manager</span></h4>
+    <h4><b>Employee ID: ${manager.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${manager.email}">${manager.email}</a></b></h4>
+    <h4><b>Office Number: ${manager.number}</b></h4>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+function appendEngineer(engineer){
+    return `
+    <div class="col-md-4">
+    <div class="card">
+    <div class="container">
+    <h2><b>${engineer.name}</b></h2>
+    <h4><span class="badge">Engineer</span></h4>
+    <h4><b>Employee ID: ${engineer.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></b></h4>
+    <h4><b>GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></b></h4>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+function appendIntern(intern){
+    return `
+    <div class="col-md-4">
+    <div class="card">
+    <div class="container">
+    <h2><b>${intern.name}</b></h2>
+    <h4><span class="badge">Intern</span></h4>
+    <h4><b>Employee ID: ${intern.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${intern.email}">${intern.email}</a></b></h4>
+    <h4><b>School: ${intern.school}</b></h4>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+function generatePage(teamMembers){
+
+    
+    const teamMemberCard = [];
+    for(let i=0; i < teamMembers.length; i++){
+        const role = teamMembers[i].getRole();
+        const member = teamMembers[i]
+        if(role === 'Manager') teamMemberCard.push(appendManager(member))
+        else if (role === 'Engineer') teamMemberCard.push(appendEngineer(member))
+        else if (role === 'Intern') teamMemberCard.push(appendIntern(member))
+    }
+    return htmlelements(teamMemberCard.join(''));
+
+};
+
+module.exports = generatePage;
